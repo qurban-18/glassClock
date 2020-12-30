ba130e0f337ea8dde6800a08fb2e658a7f6ce937
Qurban Awan <malikqurbanawan807@gmail.com>
2020-12-30 23:38:25 +0000

    first commit

---
+++ js/index.js
@@ -0,0 +1,79 @@
+let hours = document.querySelector('#hour');
+let mins = document.querySelector('#min');
+let secs = document.querySelector('#sec');
+let day = document.querySelector('#day');
+let img = document.querySelector('img');
+let main = document.querySelector('.wrapper')
+let a = 1;
+
+var randomColor = function(){
+    let sym = '0123456789ABCDEF'
+    let col = '#';
+
+    for(i=0;i<6;i++){
+        color += sym[Math.floor(Math.random() * 16)]
+    }
+}
+
+let clock = function (){
+    let time = new Date();
+    let hour = time.getHours();
+    let min = time.getMinutes();
+    let sec = time.getSeconds();
+    let days = time.getDay();
+
+    let h = (hour * 30) + (min / 2);
+    let m = (min * 6) + (sec / 10);
+    let s = sec * 6;
+
+    hours.style.transform = `rotate(${h}deg)`
+    mins.style.transform = `rotate(${m}deg)`
+    secs.style.transform = `rotate(${s}deg)`
+
+    switch(days){
+        case 1:
+            days = 'monday'
+            break;
+        case 2:
+            days = 'tusday'
+            break;
+        case 3:
+            days = 'wensday'
+            break;
+        case 4:
+            days = 'thrusday'
+            break;
+        case 5:
+            days = 'friday'
+            break;
+        case 6:
+            days = 'saturday'
+            break;
+        case 7:
+            days = 'sunday'
+            break;
+    }
+    day.innerText = days;
+
+    let sym = '0123456789ABCDEF'
+    let col = '#';
+
+    for(i=0;i<6;i++){
+        col += sym[Math.floor(Math.random() * 16)]
+        document.querySelector('.clock').style.boxShadow = `0 0 30px ${col}`;
+        day.style.boxShadow = `0 3px 5px ${col}`
+    }
+    
+}
+
+setInterval(clock, 1000);
+
+setInterval(() => {
+    let no = Math.floor(Math.random() * 4)
+    if(no<1){
+        no=1;
+        console.log(no)
+    }
+    main.style.backgroundImage = `url(imgs/bg${no}.jpg)`
+    // console.log(no)
+}, 2000);
